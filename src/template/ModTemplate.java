52f0b4bec14dc17bb2cf4547b4ecab5369f5d588
GlennFolker <glenn.folk08@gmail.com>
2023-12-17 19:14:50 +0700

    Initial commit

---
+++ src/template/ModTemplate.java
@@ -0,0 +1,11 @@
+package template;
+
+import mindustry.mod.*;
+import template.gen.*;
+
+public class ModTemplate extends Mod{
+    @Override
+    public void loadContent(){
+        EntityRegistry.register();
+    }
+}
