00226d6b45dd5241a6fee704a26c63ada83edd99
GlennFolker <glenn.folk08@gmail.com>
2024-12-09 18:07:17 +0700

    use the new GitHub repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,7 @@
 pluginManagement{
     repositories{
         gradlePluginPortal()
-        maven("https://maven.pkg.github.com/GlennFolker/EntityAnno").credentials{
-            // "Obfuscated" token for reading packages, since GitHub blocks tokens shared publicly for no reason.
-            //
-            // Do NOT deobfuscate this token (i.e., converting these 4 bytes into characters and writing them in the
-            // source file) as that will get GitHub to block it, and make *literally everybody* unable to pull the
-            // public packages.
-            //
-            // Alternatively, you could create your own GitHub classic Personal Access Token with `packages:read`
-            // permission (and *only* that) and copy the string following the `ghp_` prefix, and replace the one
-            // following the 4 bytes below with it.
-            username = "GitHub Packages"
-            password = "${String(byteArrayOf(0x67, 0x68, 0x70, 0x5f), Charsets.UTF_8)}o9b0qcWBMqta7TOpN0BggXBJtsYwyc4N4D0O"
-        }
+        maven("https://raw.githubusercontent.com/GlennFolker/EntityAnnoMaven/main")
     }
 
     plugins{
